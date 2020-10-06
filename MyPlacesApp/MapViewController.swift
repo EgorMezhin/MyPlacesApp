015dfe722a9c81a90b842da785b0e277bc49ad13
EgorMezhin <49487690+EgorMezhin@users.noreply.github.com>
2020-10-06 05:06:34 +0300

    Location updates

--- MyPlacesApp/MapViewController.swift
+++ MyPlacesApp/MapViewController.swift
@@ -8,16 +8,30 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
     
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 2000
+    var incomeSegueIdentifier = ""
     
+    @IBOutlet weak var adressLabel: UILabel!
+    @IBOutlet weak var mapPinImage: UIImageView!
+    @IBOutlet weak var doneButton: UIButton!
     
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBAction func centerViewInUserLocation() {
+        
+       showUserLocation()
+    }
+    
+    @IBAction func doneButtonPressed() {
+    }
     @IBAction func closeVC() {
         dismiss(animated: true)
     }
@@ -25,9 +39,20 @@ class MapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
-        setupPlacemark()
+        setupMapView()
+        checkLocationServices()
 
         // Do any additional setup after loading the view.
+    }
+    
+    private func setupMapView() {
+        
+        if incomeSegueIdentifier == "showPlace" {
+            setupPlacemark()
+            mapPinImage.isHidden = true
+            adressLabel.isHidden = true
+            doneButton.isHidden = true
+        }
     }
     
     private func setupPlacemark() {
@@ -58,7 +83,60 @@ class MapViewController: UIViewController {
         }
     }
 
+    private func checkLocationServices() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            setUpLocationManager()
+            checkLocationAuthorization()
+        } else {
+            DispatchQueue.main.async {
+                self.showALert(title: "Location services are disabled", message: "You can enable it in Settings")
+            }
+        }
+    }
+    
+    private func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
 
+    private func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            if incomeSegueIdentifier == "getAdress" { showUserLocation()}
+            break
+        case .denied:
+            DispatchQueue.main.async {
+                self.showALert(title: "Your location is not available", message: "Go to Settings to activate it ")
+            }
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        @unknown default:
+            print("New case is available")
+        }
+    }
+    
+    private func showUserLocation() {
+        
+        if let location = locationManager.location?.coordinate {
+                   let region = MKCoordinateRegion(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+                   
+                   mapView.setRegion(region, animated: true)
+               }
+    }
+    private func showALert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
@@ -84,4 +162,11 @@ extension MapViewController: MKMapViewDelegate {
         return annotationView
     }
     
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
 }
