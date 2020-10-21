5ba19d350597c48a3e8f7bb23b6f245d25898c42
EgorMezhin <49487690+EgorMezhin@users.noreply.github.com>
2020-10-21 14:00:28 +0300

    branch error fix

--- MyPlacesApp/Controller/AppDelegate.swift
+++ MyPlacesApp/Controller/AppDelegate.swift
@@ -12,8 +12,6 @@ import RealmSwift
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let schemaVersion: UInt64 = 3
